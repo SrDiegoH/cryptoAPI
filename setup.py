2e07524c7feb501b93c7abe84bb0de33693f2909
SrDiegoH <diegohenrique4222@gmail.com>
2022-01-06 10:59:37 -0300

    Add update python version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=find_packages(where='.', exclude=(), include=('*',)),
     classifiers=[ 'Python' ],
     install_requires=['flask', 'pycryptodome'],
-    python_requires='<3.9.7',
+    python_requires='3.9.9',
     include_package_data=True,
     install_package_data=True,
 )
